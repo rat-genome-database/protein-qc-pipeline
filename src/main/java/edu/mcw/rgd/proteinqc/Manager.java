18194e5aaad7f7126c6e29780aa56e2ce717a7f2
tutajm <mtutaj@mcw.edu>
2019-10-15 16:37:09 -0500

    merged duplicate code for dry-run and normal-run

--- src/main/java/edu/mcw/rgd/proteinqc/Manager.java
+++ src/main/java/edu/mcw/rgd/proteinqc/Manager.java
@@ -25,10 +25,10 @@ public class Manager {
         for( String arg: args ) {
             switch(arg.toLowerCase()) {
                 case "-dryrun":
-                    mode = "dryRun";
+                    mode = "DRY RUN";
                     break;
                 case "-normalrun":
-                    mode = "normalRun";
+                    mode = "NORMAL RUN";
                     break;
                 default:
                     break;
@@ -59,108 +59,78 @@ public class Manager {
      * @throws Exception *** EXTREMELY USEFUL TO FIND OUT WHAT WRONG HAPPENED TO YOUR PROGRAM *** DO NOT IGNORE THEM ***
      */
     public void run(String mode) throws Exception{
+
+        if( mode==null ) {
+            return;
+        }
+        boolean isNormalMode = mode.equals("NORMAL RUN");
+
+        log.info("MODE: "+mode);
+        log.info("=========================================================================================");
+        log.info("**PROTEIN SEQUENCE ANALYSIS TO GENERATE MULTI STOP CODON SEQUENCE DATA FOR**");
+        log.info("\t\t QUALITY CONTROL ON ALL EXISTING RGD ASSEMBLIES ****");
+        log.info("==================================================================================================================================");
+
         DAO dao = new DAO();
-        String MODE = mode.toLowerCase();
         List<Integer> mapkeys = new ArrayList<>(Arrays.asList(60, 70, 360));
-        if(MODE.equals("dryrun")){
-            log.info("MODE: DRY RUN \n=========================================================================================");
-            log.info("**PROTEIN SEQUENCE ANALYSIS TO GENERATE MULTI STOP CODON SEQUENCE DATA FOR**" +" \n\t\t" + " QUALITY CONTROL ON ALL EXISTING RGD ASSEMBLIES ****");
-            log.info("==================================================================================================================================");
-            for(int mapkey : mapkeys) {
-                String mapKeyName = dao.getMapName(mapkey);
-                log.info("GENERATING MULTI STOP CODON REPORT FOR  " + mapKeyName + "  CHROMOSOME WISE...");
-                log.info("====================================================================================================");
-
-                TranscriptAnalysisMethods transcriptAnalysisMethods = new TranscriptAnalysisMethods();
-                /*GET MULTI STOP CODON TRANSCRIPTS FOR ONE WHOLE ASSEMBLY AT A TIME*/
-                List<TranscriptData> transcriptDataList= transcriptAnalysisMethods.getMultiStopCodonTranscripts(mapkey, dao);
-                log.info("transcripts data loaded: "+transcriptDataList.size());
-                for(TranscriptData trData: transcriptDataList){
-                    Set<Integer> multistopcodonTranscritIds = new HashSet<>(trData.getMultiStopCodonTranscriptRgdIds());
-                    List<Integer> multistopcodonVariantTranscriptIds = trData.getMultiStopCoodnVariantTranscriptIds();
-                    int transcriptsSize = multistopcodonTranscritIds.size();
-                    int variantsSize= multistopcodonVariantTranscriptIds.size();
-                    int missingRefSeqCount = trData.getMissingRefSeqCount();
-                    Set<Integer> missingRefSeqIds = trData.getMissingRefSeqIds();
-                    String chr = trData.getChromosome();
-                    log.info("CHROMOSOME: " + chr);
-                    log.info("====================================================================");
-                    log.info("Multi Stop Codon GENES COUNT : "+ transcriptsSize);
-                    log.info("Multi Stop Codon VARIANT TRANSCRIPTS COUNT  : " + variantsSize);
-                    log.info("Missing RefSeq Count: " + missingRefSeqCount + "\nPlease Update the Database for Missing RefSeq and Run this CODE again.\nSee the MAIN LOG FOR MORE INFO. ");
-                    log.info("**********MISSING REFSEQ TRANSCRIPT RGD IDS and SYMBOLS***********");
-                    for(int id: missingRefSeqIds){
-                        Genes genes = dao.getGeneSymbols(id);
-                        String geneSymbol= genes.getGeneSymbol();
-                        log.info("                " + id + "                 " + geneSymbol);
-                    }
-
-                    if(multistopcodonTranscritIds.size()==0){
-                        continue;
-                    }else{
-                        log.info("******MULTI STOP CODON TranscriptRGDIds and GENE Symbols on Chromosome: " + chr +"**********");
-                        log.info("Transcript RGDID **** GENE SYMBOL ***** PROTEIN ACC ID");
-                        for(int trId:multistopcodonTranscritIds){
-                            Genes genes = dao.getGeneSymbols(trId);
-                            String geneSymbol = genes.getGeneSymbol();
-                            String proteinAccId=genes.getProteinAccId();
-                            log.info("        " + trId + "        " + geneSymbol + "       " + proteinAccId);
-                        }
-                    }
+        for(int mapkey : mapkeys) {
+            String mapKeyName = dao.getMapName(mapkey);
+            log.info("GENERATING MULTI STOP CODON REPORT FOR  " + mapKeyName + "  CHROMOSOME WISE...");
+            log.info("====================================================================================================");
+            TranscriptAnalysisMethods transcriptAnalysisMethods = new TranscriptAnalysisMethods();
+            // GET MULTI STOP CODON TRANSCRIPTS FOR ONE WHOLE ASSEMBLY AT A TIME
+            List<TranscriptData> transcriptDataList= transcriptAnalysisMethods.getMultiStopCodonTranscripts(mapkey, dao);
+            log.info("transcripts data loaded: "+transcriptDataList.size());
+            for(TranscriptData trData: transcriptDataList) {
+                String chr = trData.getChromosome();
+                log.info("CHROMOSOME: " + chr);
+                log.info("====================================================================");
+                List<Integer> multiStopCodonVariantTranscriptIds = trData.getMultiStopCoodnVariantTranscriptIds();
+                Set<Integer> missingRefSeqIds = trData.getMissingRefSeqIds();
+                int missingRefSeqCount = trData.getMissingRefSeqCount();
+                int multistopcodonVariantCount = multiStopCodonVariantTranscriptIds.size();
+                int multistopcodonGeneCount = trData.getMultiStopCodonTranscriptRgdIds().size();
+                log.info("TOTAL NUMBER OF MULTI STOP CODON SEQUENCES OF CHROMOSOME " + chr + " : " + multistopcodonVariantCount);
+                log.info("Multi Stop Codon GENE COUNT" + multistopcodonGeneCount);
+                log.info("Missing RefSeq Count: " + missingRefSeqCount);
+                log.info("Please Update the Database for Missing RefSeq and Run this CODE again.");
+                log.info("See the MAIN LOG FOR MORE INFO. ");
+                log.info("**********MISSING REFSEQ TRANSCRIPT RGD IDS and SYMBOLS***********");
+                for (int id : missingRefSeqIds) {
+                    Genes genes = dao.getGeneSymbols(id);
+                    String geneSymbol = genes.getGeneSymbol();
+                    log.info("                " + id + "                 " + geneSymbol);
                 }
-            }
-            log.info("REPORT GENERATION COMPLETED.\n dryRun completed Successfully");
+                if (multistopcodonVariantCount == 0) {
+                    continue;
+                } else {
+                    log.info("******MULTI STOP CODON TranscriptRGDIds and GENE Symbols on Chromosome: " + chr +"**********");
+                    log.info("Transcript RGDID **** GENE SYMBOL ***** PROTEIN ACC ID");
 
-        }else if(MODE.equals("normalrun")){
-            log.info("MODE: NORMAL RUN\n====================================================================");
-            log.info("**PROTEIN SEQUENCE ANALYSIS TO GENERATE MULTI STOP CODON SEQUENCE DATA FOR**" + " \n\t\t" + " QUALITY CONTROL ON ALL EXISTING RGD ASSEMBLIES ****");
-            log.info("=================================================================================================================================");
-            for(int mapkey : mapkeys) {
-                String mapKeyName=dao.getMapName(mapkey);
-                log.info("GENERATING MULTI STOP CODON REPORT FOR  " + mapKeyName + "  CHROMOSOME WISE...");
-                log.info("=========================================================================================");
-                TranscriptAnalysisMethods transcriptAnalysisMethods = new TranscriptAnalysisMethods();
-                List<TranscriptData> transcriptDataListAllChromosomes= transcriptAnalysisMethods.getMultiStopCodonTranscripts(mapkey, dao);
-                for(TranscriptData trDataEachChromosome: transcriptDataListAllChromosomes) {
-                    String chr = trDataEachChromosome.getChromosome();
-                    log.info("CHROMOSOME: " + chr + "\n====================================================================");
-                    List<Integer> multiStopCodonVariantTranscriptIds = trDataEachChromosome.getMultiStopCoodnVariantTranscriptIds();
-                    Set<Integer> missingRefSeqIds = trDataEachChromosome.getMissingRefSeqIds();
-                    int multistopcodonVariantCount = multiStopCodonVariantTranscriptIds.size();
-                    int multistopcodonGeneCount = trDataEachChromosome.getMultiStopCodonTranscriptRgdIds().size();
-                    int missingRefSeqCount = trDataEachChromosome.getMissingRefSeqCount();
-                    log.info("TOTAL NUMBER OF MULTI STOP CODON SEQUENCES OF CHROMOSOME " + chr + " : " + multistopcodonVariantCount);
-                    log.info("Multi Stop Codon GENE COUNT" + multistopcodonGeneCount);
-                    log.info("Multi Stop Codon VARIANT SEQ COUNT" + multistopcodonVariantCount);
-                    log.info("TOTAL NUMBER OF MULTI STOP CODON SEQUENCES OF CHROMOSOME" + chr + ": " + multistopcodonVariantCount);
-                    log.info("Missing REFSEQ COUNT: " + missingRefSeqCount + "\nPlease Update the Database for Missing RefSeq and Run this CODE again.\nSee the MAIN LOG FOR MORE INFO. ");
-                    log.info("**********MISSING REFSEQ TRANSCRIPT RGD IDS and SYMBOLS***********");
-                    for (int id : missingRefSeqIds) {
-                        Genes genes = dao.getGeneSymbols(id);
-                        String geneSymbol = genes.getGeneSymbol();
-                        log.info("                " + id + "                 " + geneSymbol);
+                    Set<Integer> multistopcodonTranscritIds =trData.getMultiStopCodonTranscriptRgdIds();
+                    for(int trId:multistopcodonTranscritIds){
+                        Genes genes = dao.getGeneSymbols(trId);
+                        String geneSymbol=genes.getGeneSymbol();
+                        String proteinAccId= genes.getProteinAccId();
+                        log.info("        " + trId + "        " + geneSymbol + "       " + proteinAccId);
                     }
-                    if (multistopcodonVariantCount == 0) {
-                        continue;
-                    } else {
-                        log.info("******MULTI STOP CODON TranscriptRGDIds and GENE Symbols on Chromosome: " + chr +"**********");
-                        log.info("Transcript RGDID **** GENE SYMBOL ***** PROTEIN ACC ID");
-                        Set<Integer> multistopcodonTranscritIds =trDataEachChromosome.getMultiStopCodonTranscriptRgdIds();
-                        for(int trId:multistopcodonTranscritIds){
-                            Genes genes = dao.getGeneSymbols(trId);
-                            String geneSymbol=genes.getGeneSymbol();
-                            String proteinAccId= genes.getProteinAccId();
-                            log.info("        " + trId + "        " + geneSymbol + "       " + proteinAccId);
-                        }
-                        log.debug("UPDATING THE VARIANT TRANSCRIPT DATA of CHROMOSOME: " +chr + ".....");
-                        for(int variantTranscriptId: multiStopCodonVariantTranscriptIds){
+
+                    if( isNormalMode ) {
+                        log.debug("UPDATING THE VARIANT TRANSCRIPT DATA of CHROMOSOME: " + chr + ".....");
+                        for (int variantTranscriptId : multiStopCodonVariantTranscriptIds) {
                             dao.updateVariantTranscript(variantTranscriptId);
                         }
                         log.info("UPDATE COMPLETED FOR CHROMOSOME: " + chr + "See the MAIN LOG FOR UPDATED GENEs LIST");
                     }
                 }
             }
+        }
+
+        if( isNormalMode ) {
             log.info("DATABASE UPDATE COMPLETED ON ALL ASSEBLIES FOR REPORTED LIST OF GENES OF MULTISTOP CODON TRANSCRIPTS");
+        } else {
+            log.info("REPORT GENERATION COMPLETED.");
+            log.info(" dryRun completed Successfully");
         }
     }
 
